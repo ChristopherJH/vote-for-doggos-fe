2003a393a5b6df5c6f724d4fdcff39d3428d7363
Jo <joely.rooke@gmail.com>
2021-12-14 17:11:15 +0000

    Create helper function to create the url format for dog breed

---
+++ src/utils/urlDogBreed.ts
@@ -0,0 +1,30 @@
+export default function urlDogBreeds(breed: string): string {
+  // Whippet --> whippet
+  // Patterdale Terrier --> terrier/patterdale
+  let urlBreed = "";
+  if (!breed.includes(" ")) {
+    //if the breed is a single word
+    urlBreed = `${breed.slice(0, 1).toLowerCase()}${breed.slice(
+      1,
+      breed.length
+    )}`; // decapitalise it
+  } else {
+    // if the breed has a sub-breed
+    let breedArr = breed.split(" ");
+    let decapArr = [];
+    for (let part of breedArr) {
+      let decapitalised = `${part.slice(0, 1).toLowerCase()}${part.slice(
+        1,
+        part.length
+      )}`; // decapitalise each word
+      decapArr.push(decapitalised);
+    }
+    let orderedArr = [];
+    for (let i = decapArr.length - 1; i >= 0; i--) {
+      // re-order the words
+      orderedArr.push(decapArr[i]);
+    }
+    urlBreed = orderedArr.join("/"); // format the words into a string separated by a "/"
+  }
+  return urlBreed;
+}

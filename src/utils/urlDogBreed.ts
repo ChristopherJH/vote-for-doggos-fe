845c79e9874cab4bf38f97dcebc4d048e19da748
Jo <joely.rooke@gmail.com>
2021-12-14 17:28:26 +0000

    changed let to constant variables

--- src/utils/urlDogBreed.ts
+++ src/utils/urlDogBreed.ts
@@ -10,16 +10,16 @@ export default function urlDogBreeds(breed: string): string {
     )}`; // decapitalise it
   } else {
     // if the breed has a sub-breed
-    let breedArr = breed.split(" ");
-    let decapArr = [];
-    for (let part of breedArr) {
-      let decapitalised = `${part.slice(0, 1).toLowerCase()}${part.slice(
+    const breedArr = breed.split(" ");
+    const decapArr = [];
+    for (const part of breedArr) {
+      const decapitalised = `${part.slice(0, 1).toLowerCase()}${part.slice(
         1,
         part.length
       )}`; // decapitalise each word
       decapArr.push(decapitalised);
     }
-    let orderedArr = [];
+    const orderedArr = [];
     for (let i = decapArr.length - 1; i >= 0; i--) {
       // re-order the words
       orderedArr.push(decapArr[i]);
